f5a7275a496a99feaf10db7ae7e70fba7805afb2
Antony MECHIN <antony.mechin@docker.com>
2017-12-17 22:31:59 +0100

    Initialize.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

---
+++ spec/ee-pipeline-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EePipelineView from '../lib/ee-pipeline-view';
+
+describe('EePipelineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
